3f8d9b0cc65f6b51becc28c2a8aed3e7b2e3cc0c
Your Name <timon_a@hotmail.com>
2021-03-25 09:41:23 +0100

    added Router and Link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route,} from "react-router-dom";
 import TopMenu from './components/TopMenu';
 import HomePage from './pages/Home';
 import CavitiesPage from './pages/Cavities';
@@ -7,15 +8,26 @@ import WhiteningPage from './pages/Whitening';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <TopMenu />
-      <HomePage />
-      <CavitiesPage />
-      <AppointmentsPage />
-      <WhiteningPage />
-    </>
-  );
+    return (
+        <Router>
+            <TopMenu/>
+            <Switch>
+                <Route exact path='/'>
+                    <HomePage/>
+                </Route>
+                <Route exact path='/gaatjes'>
+                    <CavitiesPage/>
+                </Route>
+                <Route exact path='/afspraken'>
+                    <AppointmentsPage/>
+                </Route>
+                <Route exact path='/bleken'>
+                    <WhiteningPage/>
+                </Route>
+
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
